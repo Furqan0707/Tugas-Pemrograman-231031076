b022ad42e02a43f4e31eed368dfde994691a07a9
Furqan0707 <153783484+Furqan0707@users.noreply.github.com>
2023-12-21 18:39:09 +0800

    Add files via upload

---
+++ 231031076-Furqan-Tugas-9-c.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "M Furqan Ramadhanu",
+      "kelas" : "SI23-C",
+      "nim" : "231031076",
+      "asal" : "Sidenreng Rappang",
+      "tanggal" : "10-Oktober-2004",
+      "alamat" : "Belawae",
+      "hobi" : "Main Volly",
+      "tinggi" : "165 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
